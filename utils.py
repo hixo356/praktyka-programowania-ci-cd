e003fda66a6bbbae7accef888946812eeaa71231
hixo356 <piotrek.bartczak356@gmail.com>
2024-05-08 14:35:09 +0200

    Black fix

--- utils.py
+++ utils.py
@@ -1,17 +1,21 @@
 """Module with math operations"""
 
+
 def add(a: int, b: int) -> int:
     """Returns sum of two numbers"""
-    return a+b
+    return a + b
+
 
 def subtract(a: int, b: int) -> int:
     """Returns difference between two numbers"""
-    return a-b
+    return a - b
+
 
 def multiply(a: int, b: int) -> int:
     """Returns product of two numbers"""
-    return a*b
+    return a * b
+
 
 def divide(a: int, b: int) -> float:
     """Returns quotient of two numbers"""
-    return a/b
+    return a / b
